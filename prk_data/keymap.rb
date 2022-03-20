d86957072cf847498b135716dfa50e2c35332db9
MachiaWorks <MachiaWorks@users.noreply.github.com>
2022-03-20 21:53:34 +0900

    update.

--- prk_data/keymap.rb
+++ prk_data/keymap.rb
@@ -3,6 +3,7 @@
 # Initialize a Keyboard
 kbd = Keyboard.new
 
+
 r0 = 15
 r1 = 14
 r2 = 13
@@ -30,43 +31,37 @@ kbd.init_matrix_pins(
 
 # default layer should be added at first
 kbd.add_layer :default, %i(
-  KC_Q    KC_W   KC_E  KC_R   KC_T           KC_Y  KC_U   KC_I   KC_O   KC_P  
+  Q_GUI    KC_W   KC_E  KC_R   KC_T           KC_Y  KC_U   KC_I   KC_O   KC_P  
   KC_A    KC_S   KC_D  KC_F   KC_G           KC_H  KC_J   KC_K   KC_L   KC_BSPC  
-  KC_Z    KC_X   KC_C  KC_V   KC_B           KC_N  KC_M   KC_UP   KC_ENTER   KC_NO  
-  KC_LSFT  KC_LALT  KC_LCTL  SPC_RAISE  KC_SPACE     KC_RCTL  KC_LEFT   KC_DOWN   KC_RIGHT   KC_NO 
+  Z_LSFT    KC_X   KC_C  KC_V   KC_B           KC_N  KC_M   KC_UP   KC_ENTER   KC_NO  
+  KC_ESC  KC_LALT  KC_LCTL  SPC_RAISE  SPC_LOWER     KC_RCTL  KC_LEFT   KC_DOWN   KC_RIGHT   KC_NO 
 ) 
 
 kbd.add_layer :raise, %i(
   KC_1      KC_2   KC_3   KC_4   KC_5               KC_6  KC_7   KC_8   KC_9   KC_0  
-  KC_TRANSPARENT      KC_S   KC_D   KC_SCOLON   KC_QUOT     KC_H  KC_RBRC   KC_BSLS   KC_NO   KC_ENTER  
-  Z_LSFT      KC_X   KC_C   KC_INT1   KC_BBLS          KC_SLSH  KC_COMM   KC_PGUP   KC_PGUP   KC_NO 
-  KC_LSFT     KC_LALT   KC_LCTL   KC_NO   KC_NO         KC_RCTL  KC_HOME   KC_PGDN   KC_END   KC_NO
+  KC_TRANSPARENT      KC_S   KC_D   KC_SCOLON   KC_QUOT     KC_H  KC_RBRC   KC_BSLS   KC_NO   KC_DEL 
+  KC_Z      KC_X   KC_C   KC_INT1   KC_BBLS          KC_SLSH  KC_COMM   KC_PGUP   KC_ENTER   KC_NO 
+  KC_TAB     KC_LALT   KC_LCTL   KC_NO   KC_INT5         KC_RCTL  KC_HOME   KC_PGDN   KC_END   KC_NO
 )
 
 kbd.add_layer :lower, %i(
-  KC_ESC      KC_F1   KC_F2   KC_F3   KC_F4         KC_Y  KC_U   KC_I   KC_DEL   KC_BSPC  
-  A_RCT      KC_F5   KC_F6   KC_7   KC_8         KC_H  KC_J   KC_K   KC_NO   KC_ENTER  
-  Z_SFT      KC_F9   KC_F10  KC_F11   KC_F12     KC_MINS  KC_N   KC_M   KC_PGUP   KC_END  
-  KC_ESC      KC_RALT   KC_2   KC_ENTER  KC_NO    KC_NO  KC_3   KC_HOME   KC_NO   KC_PGDN
+  KC_F1      KC_F2   KC_F3   KC_F4 KC_F5          KC_F6   KC_F7   KC_F8 KC_F9 KC_F10      
+  A_RCT      KC_F11   KC_F12   KC_F   KC_G         KC_H  KC_RBRC   KC_BSLS   KC_NO   KC_DEL  
+  Z_SFT      KC_X   KC_C  KC_INT1   KC_BBLS     KC_SLSH  KC_COMM   KC_PGUP   KC_ENTER   KC_NO  
+  KC_TAB      KC_LALT   KC_LCTL   KC_INT4   KC_NO      KC_RCTL  KC_HOME   KC_PGDN   KC_END   KC_NO
 )
 
-kbd.add_layer :normal, %i(
-  KC_GRV      KC_W   KC_E   KC_R   KC_T             KC_Y  KC_U   KC_I   KC_O   KC_P  
-  KC_A      KC_S   KC_RSFT   KC_RCTL   KC_RALT    KC_RCTL  KC_RSFT   KC_RALT   KC_NO   KC_ENTER  
-  KC_Z      KC_X   KC_C   KC_INT1   KC_B       KC_NO  KC_COMM   KC_DOT   KC_PGUP   KC_END  
-  KC_TAB  KC_LALT  KC_5  KC_ENTER   KC_NO         KC_ENTER  KC_1   KC_HOME   KC_NO   KC_PGDN
-)
+
 #
 #                   Your custom    Keycode or                          Keycode (only modifiers)    Release time     Re-push time
 #                   key name       Array of Keycode                    or Layer Symbol to be held  threshold(ms)    threshold(ms)
 #                                  or Proc                             or Proc which will run      to consider as   to consider as
 #                                  when you click                      while you keep press        `click the key`  `hold the key`
 kbd.define_mode_key :Z_LSFT,    [ :KC_Z,                               :KC_LSFT,                   150,             150 ]
-kbd.define_mode_key :A_LCT,    [ :KC_A,                               :KC_LCTL,                   150,             150 ]
+kbd.define_mode_key :Q_GUI, [ :KC_Q, :KC_LGUI, 150, 150]
 
 kbd.define_mode_key :SPC_RAISE, [ :KC_SPACE,                           :raise,                     150,             150 ]
 kbd.define_mode_key :SPC_LOWER, [ :KC_SPACE,                           :lower,                     150,             150 ]
-kbd.define_mode_key :MNS_NORMAL, [ :KC_MINS,                              :normal,                     150,             150 ]
 
 
 kbd.start!
