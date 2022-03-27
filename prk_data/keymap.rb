6a736e6412b88cf601c1e5e1e1e5ba8ee4fadbce
MachiaWorks <MachiaWorks@users.noreply.github.com>
2022-03-27 10:40:43 +0900

    Update(3layer use )

--- prk_data/keymap.rb
+++ prk_data/keymap.rb
@@ -39,15 +39,15 @@ kbd.add_layer :default, %i(
 
 kbd.add_layer :raise, %i(
   KC_1      KC_2   KC_3   KC_4   KC_5               KC_6  KC_7   KC_8   KC_9   KC_0  
-  KC_TRANSPARENT      KC_S   KC_D   KC_SCOLON   KC_QUOT     KC_H  KC_RBRC   KC_BSLS   KC_NO   KC_DEL 
-  KC_Z      KC_X   KC_C   KC_INT1   KC_BBLS          KC_SLSH  KC_COMM   KC_PGUP   KC_ENTER   KC_NO 
+  KC_A      KC_S   KC_D   KC_SCOLON   KC_QUOT     KC_H  KC_RBRC   KC_BSLS   KC_MINUS   KC_DEL 
+  Z_LSFT      KC_X   KC_INT1   KC_BBLS   KC_SLSH          KC_COMM  KC_DOT   KC_PGUP   KC_ENTER   KC_NO 
   KC_TAB     KC_LALT   KC_LCTL   KC_NO   KC_INT5         KC_RCTL  KC_HOME   KC_PGDN   KC_END   KC_NO
 )
 
 kbd.add_layer :lower, %i(
   KC_F1      KC_F2   KC_F3   KC_F4 KC_F5          KC_F6   KC_F7   KC_F8 KC_F9 KC_F10      
-  A_RCT      KC_F11   KC_F12   KC_F   KC_G         KC_H  KC_RBRC   KC_BSLS   KC_NO   KC_DEL  
-  Z_SFT      KC_X   KC_C  KC_INT1   KC_BBLS     KC_SLSH  KC_COMM   KC_PGUP   KC_ENTER   KC_NO  
+  KC_GRAVE   KC_F11   KC_F12   KC_F   KC_G         KC_H  KC_RBRC   KC_BSLS   KC_MINUS   KC_DEL  
+  Z_LSFT      KC_X   KC_INT1  KC_BBLS   KC_SLSH     KC_COMM  KC_DOT   KC_PGUP   KC_ENTER   KC_NO  
   KC_TAB      KC_LALT   KC_LCTL   KC_INT4   KC_NO      KC_RCTL  KC_HOME   KC_PGDN   KC_END   KC_NO
 )
 
